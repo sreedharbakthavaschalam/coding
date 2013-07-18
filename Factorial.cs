bec5a2a915e7336ee5cada0860ec17b84df35b33
sreedharbakthavaschalam <sreedhar.bakthavaschalam@iopextech.com>
2013-07-19 04:53:51 +0530

    Update Factorial.cs

--- Factorial.cs
+++ Factorial.cs
@@ -1,4 +1,47 @@
-coding
-======
+using System.IO;
 
-Coding
+using System;
+
+ 
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        // Read in every line in the file.
+
+        using (StreamReader reader = new StreamReader("input.txt"))
+
+        {
+
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+
+            {
+
+                int number = Convert.ToInt32(line);
+
+                int fact = 1;
+
+                for(int i=1;i<=number;i++) {
+
+                    fact = fact*i;
+
+                }
+
+                Console.WriteLine(fact);
+
+            }
+
+           
+
+        }
+
+    }
+
+}
